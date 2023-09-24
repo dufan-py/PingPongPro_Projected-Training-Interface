30f50ccd1dde0668f6bae2f122fc23c7e942d02c
dufan-py <925062737@qq.com>
2023-09-24 19:35:09 +0800

    s

--- Axis_Change.py
+++ Axis_Change.py
@@ -1,38 +1,58 @@
 import cv2
 import numpy as np
+import random
+
+def get_random_coordinates():
+    x = random.randint(-460, 460)  # 您可以自定义范围
+    y = random.randint(-250, 250)  # 您可以自定义范围
+    return x, y
+
+def get_coordinates():
+
+    # 定义原始的矩形Q和任意四边形W的坐标
+    halfWidth = 2740 / 2 / 2/1.5
+    halfHeight = 1525 / 2 / 2/1.5
+
+    Q = np.float32([
+        [-halfWidth, -halfHeight],
+        [halfWidth, -halfHeight],
+        [halfWidth, halfHeight],
+        [-halfWidth, halfHeight]
+    ])
+
+#  ！！！改四角坐标的地方！！！
+    W = np.float32([
+        [-halfWidth, -halfHeight],
+        [halfWidth , -halfHeight],
+        [halfWidth/2, halfHeight],
+        [-halfWidth/2, halfHeight]
+    ])
+#  ！！！改四角坐标的地方！！！
+
+    K = get_random_coordinates()
+
+    # 使用cv2.getPerspectiveTransform计算透视变换矩阵
+    M = cv2.getPerspectiveTransform(Q, W)
 
 
-# 定义原始的矩形Q和任意四边形W的坐标
-halfWidth = 550 * 1.8 / 2
-halfHeight = 550 / 2
-
-Q = np.float32([
-    [-halfWidth, -halfHeight],
-    [halfWidth, -halfHeight],
-    [halfWidth, halfHeight],
-    [-halfWidth, halfHeight]
-])
-
-W = np.float32([
-    [-halfWidth, -halfHeight + 50],
-    [halfWidth + 50, -halfHeight],
-    [halfWidth, halfHeight],
-    [-halfWidth, halfHeight - 50]
-])
+    # 对乒乓球的坐标K进行透视变换，得到L
+    K_ = np.array([[K[0], K[1]]], dtype=np.float32)
+    L = cv2.perspectiveTransform(K_.reshape(-1, 1, 2), M)
+    point = [0,0]
+    point[0] = L[0][0][0]
+    point[1] = L[0][0][1]
 
 
+    W_ = []
 
-K = get_random_coordinates();
+    # 通过循环将W的内容复制到W_中
+    for x in range(W.shape[0]):
+        row = []
+        for y in range(W.shape[1]):
+            row.append(W[x, y])
+        W_.append(row)
 
-# Q和W是已经定义好的四边形的四个角的坐标
-pts1 = np.float32(Q)  # Q的坐标，例如：[[0,0],[rectWidth,0],[rectWidth,rectHeight],[0,rectHeight]]
-pts2 = np.float32(W)  # W的坐标，例如：[[0,50],[rectWidth+50,0],[rectWidth,rectHeight-50],[0,rectHeight]]
 
-# 使用cv2.getPerspectiveTransform计算透视变换矩阵
-M = cv2.getPerspectiveTransform(pts1, pts2)
+    return point, W_
 
-# 对乒乓球的坐标K进行透视变换，得到L
-K_ = np.array([[[K.x, K.y]]], dtype='float32')  # 假设K有x和y属性
-L_ = cv2.perspectiveTransform(K_, M)
 
-# L_中的值即为变换后的乒乓球坐标L
--- app01/views.py
+++ app01/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, HttpResponse
 from selenium import webdriver
 import json
 import sys
+
+import numpy as np
+from Axis_Change import get_coordinates as axis_change_coordinates
+
+
 sys.path.append("..")  # 添加项目根目录到系统路径
 from random_coordinates import get_random_coordinates  # 导入 get_random_coordinates 函数
 
@@ -9,11 +14,17 @@ def index(request):
     return render(request, "user_list.html")
 
 def get_coordinates(request):
-    x, y = get_random_coordinates()  # 调用 get_random_coordinates 函数获取随机 x 和 y 坐标
+
+    point, corner = axis_change_coordinates()  # 调用 get_random_coordinates 函数获取随机 x 和 y 坐标
 
     # 将数据转换为JSON格式并返回
 
-    response_data = {'x': x, 'y': y}
+    response_data = {'x': point[0], 'y': point[1],
+                     'cor1x': corner[2][0], 'cor1y': corner[2][1], 'cor2x': corner[3][0], 'cor2y': corner[3][1],
+                     'cor3x': corner[0][0], 'cor3y': corner[0][1], 'cor4x': corner[1][0], 'cor4y': corner[1][1]}
+    for key in response_data:
+        if isinstance(response_data[key], np.float32):
+            response_data[key] = float(response_data[key])
+
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
-# 【**重要**】，连接py和html的地方。
