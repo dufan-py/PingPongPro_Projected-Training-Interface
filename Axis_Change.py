e1d44ababb39fd27d9eac0dde1dc13f3ebfb56f5
dufan-py <925062737@qq.com>
2023-11-08 13:04:29 +0800

    11.8

--- Axis_Change.py
+++ Axis_Change.py
@@ -12,7 +12,8 @@ def get_coordinates():
     # 定义原始的矩形Q和任意四边形W的坐标
     halfWidth = 2740 / 2 / 2/1.5
     halfHeight = 1525 / 2 / 2/1.5
-
+    # halfWidth = 900
+    # halfHeight = 600
     Q = np.float32([
         [-halfWidth, -halfHeight],
         [halfWidth, -halfHeight],
@@ -20,27 +21,80 @@ def get_coordinates():
         [-halfWidth, halfHeight]
     ])
 
-#  ！！！改四角坐标的地方！！！
+# 改四角坐标的地方----------------------------
     W = np.float32([
         [-halfWidth, -halfHeight],
         [halfWidth , -halfHeight],
-        [halfWidth/2, halfHeight],
+        [halfWidth/1.5, halfHeight/1.3],
         [-halfWidth/2, halfHeight]
     ])
-#  ！！！改四角坐标的地方！！！
+# 改四角坐标的地方-----------------------------
 
-    K = get_random_coordinates()
+
+
+
 
     # 使用cv2.getPerspectiveTransform计算透视变换矩阵
     M = cv2.getPerspectiveTransform(Q, W)
 
+    k =[[0,0]]
+    point=[]
+    LandPoint=[]
+    k[0] = get_random_coordinates()
+
+    for i in range(0, 9):
+        k.append( [k[i][0]+15,k[i][1]+15] )
+
+# TODO：把k中坐标变成后端提供的轨迹坐标
+# TODO：把j中坐标变成后端提供的落点坐标
+    j = []
+    j.append([-100, -30])
+    for i in range(0,9):
+        j.append( [j[i][0]+30,j[i][1]] )
+
+    for i in range(0,10):
+        k_ = np.array([[k[i][0], k[i][1]]], dtype=np.float32) #转变成NumPy数组加快计算
+        L = cv2.perspectiveTransform(k_.reshape(-1, 1, 2), M) #透视变换
+        point.append([L[0][0][0], L[0][0][1]])
+    for i in range(0,10):
+        j_ = np.array([[j[i][0], j[i][1]]], dtype=np.float32)
+        L = cv2.perspectiveTransform(j_.reshape(-1, 1, 2), M)
+        LandPoint.append([L[0][0][0], L[0][0][1]])
 
     # 对乒乓球的坐标K进行透视变换，得到L
-    K_ = np.array([[K[0], K[1]]], dtype=np.float32)
-    L = cv2.perspectiveTransform(K_.reshape(-1, 1, 2), M)
-    point = [0,0]
-    point[0] = L[0][0][0]
-    point[1] = L[0][0][1]
+    # K_ = np.array([[K[0], K[1]]], dtype=np.float32)  #NumPy数组
+    # L = cv2.perspectiveTransform(K_.reshape(-1, 1, 2), M)
+    # point = [0,0]
+    # point[0] = L[0][0][0]
+    # point[1] = L[0][0][1]
+
+
+
+# 计算12个等分点的坐标--------------------------------
+    division_points = []  #二维列表
+
+    # 在宽度上的10个点
+    for i in range(1, 6):
+        x = -halfWidth + (2*halfWidth/6) * i
+        division_points.append([x, halfHeight])
+        division_points.append([x, -halfHeight])
+
+    # 在高度上的2个点
+    division_points.append([-halfWidth, 0])
+    division_points.append([halfWidth, 0])
+
+    # 对这12个点进行透视变换
+    division_points_ = np.array(division_points, dtype=np.float32)
+    transformed_points = cv2.perspectiveTransform(division_points_.reshape(-1, 1, 2), M).reshape(-1, 2).tolist()
+
+
+    P = []
+    # 格式化transformed_points的内容并存储在P中
+    for i in range(0, 12):
+
+        P.append(transformed_points[i])   #!在改P的结构
+#--------------------------------------------------
+
 
 
     W_ = []
@@ -53,6 +107,16 @@ def get_coordinates():
         W_.append(row)
 
 
-    return point, W_
+
+
+# 原点变换------------------------------------
+    origin_ = np.array([0,0], dtype=np.float32)
+    origin__ = cv2.perspectiveTransform(origin_.reshape(-1, 1, 2), M)
+    origin=[0,0]
+    origin[0] = origin__[0][0][0]
+    origin[1] = origin__[0][0][1]
+#--------------------------------------------
+
+    return point, LandPoint, W_ ,P , origin
 
 
--- app01/views.py
+++ app01/views.py
@@ -15,13 +15,26 @@ def index(request):
 
 def get_coordinates(request):
 
-    point, corner = axis_change_coordinates()  # 调用 get_random_coordinates 函数获取随机 x 和 y 坐标
+    point, LandPiont, corner, P, origin = axis_change_coordinates()  # 调用 Axis_Change  获取变换后的 x 和 y 坐标
 
     # 将数据转换为JSON格式并返回
-
-    response_data = {'x': point[0], 'y': point[1],
+    # 当前端通过 AJAX 请求后端以获取数据时，常用的数据交换格式是 JSON。
+    # JSON存储方式类似字典，但其是跨语言的数据格式
+    response_data = { 'x0':origin[0], 'y0':origin[1],
                      'cor1x': corner[2][0], 'cor1y': corner[2][1], 'cor2x': corner[3][0], 'cor2y': corner[3][1],
                      'cor3x': corner[0][0], 'cor3y': corner[0][1], 'cor4x': corner[1][0], 'cor4y': corner[1][1]}
+    for i in range(len(P)): #12个等分点
+        response_data[f'P{i}x'] = P[i][0]
+        response_data[f'P{i}y'] = P[i][1]
+
+    for i in range(len(point)): #乒乓球轨迹坐标
+        response_data[f'point{i}x'] = point[i][0]
+        response_data[f'point{i}y'] = point[i][1]
+    for i in range(len(LandPiont)):
+        response_data[f'LandPoint{i}x'] = LandPiont[i][0]
+        response_data[f'LandPoint{i}y'] = LandPiont[i][1]
+
+    # 将数据中的 np.float32 类型转换为 float 类型，进而转为 JSON 格式
     for key in response_data:
         if isinstance(response_data[key], np.float32):
             response_data[key] = float(response_data[key])
